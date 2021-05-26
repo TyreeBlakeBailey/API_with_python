d7ff14483f90bcab5bd444fec6dac401d4025436
Tyree <t9tyree@hotmail.co.uk>
2021-05-26 15:07:22 +0100

    basic API checker

--- Python_API.py
+++ Python_API.py
@@ -2,3 +2,18 @@
 # Dependencies are pip
 
 import requests
+import json
+#
+# response_bbc = requests.get("https://www.bbc.co.uk/")
+# print(response_bbc)
+# print(response_bbc.status_code)
+# print(response_bbc.headers)
+
+
+post_api_response = requests.get("https://api.postcodes.io/postcodes/se120nb")
+print(post_api_response.status_code)
+if post_api_response.status_code == 200:
+    print(f"Thanks for your request {post_api_response.status_code}")
+else:
+    print("Sorry the postcode is incorrect ")
+
